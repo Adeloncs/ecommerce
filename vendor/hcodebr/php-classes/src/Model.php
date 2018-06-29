6978e8a9a10ae3599e8acbb5b2d08420d0d9b2e1
Adelon Carneiro da Silva <adeloncs@hotmail.com>
2018-06-29 16:46:49 -0300

    tela de login

---
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -0,0 +1,48 @@
+<?php 
+
+namespace Hcode;
+
+/**
+ * 
+ */
+class Model 
+{
+	
+	private $values = [];
+
+	public function __call($name, $args) // args for pass
+	{
+		
+		$method = substr($name, 0, 3);
+		$fieldName = substr($name, 3, strlen($name));
+
+		switch ($method) // for switch sets and gets
+		{
+			case 'get':
+					return $this->values[$fieldName];
+				break;
+
+			case 'set':
+					$this->values[$fieldName] = $args[0];
+				break;
+
+		}
+
+	}
+
+	public function setData($data = array())
+	{
+		foreach ($data as $key => $value) {
+			
+			$this->{"set".$key}($value); //{} para realizar um merge
+
+		}
+	}
+
+	public function getValues()
+	{
+		return $this->values;
+	}
+}
+
+ ?>
