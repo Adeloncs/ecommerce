2b9b0c352c88308a5a2aee1ecaa589fbec0df2ce
Adelon Carneiro da Silva <adeloncs@hotmail.com>
2018-07-09 19:18:16 -0300

    Site - Categories

--- index.php
+++ index.php
@@ -333,6 +333,25 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 	exit;
 
 });
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>[]
+
+	]);
+
+
+});
+
+
  //////
 
 $app->run();
