abebffe76dfc2406fdfe0c76104dbd8e439f6fb4
Adelon Carneiro da Silva <adeloncs@hotmail.com>
2018-07-19 01:16:40 -0300

    Categorias vs produtos

--- site.php
+++ site.php
@@ -1,6 +1,7 @@
 <?php 
 
 use \Hcode\Page;
+use \Hcode\Model\Category;
 use \Hcode\Model\Product;
 
 $app->get('/', function() { // rota principal
@@ -15,6 +16,20 @@ $app->get('/', function() { // rota principal
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
 
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>Product::checkList($category->getProducts())
+
+	]);
+
+});
 
  ?>
