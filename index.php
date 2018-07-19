0d747205b1a04f1429df596b8ff0889b2a3edb3e
Adelon Carneiro da Silva <adeloncs@hotmail.com>
2018-07-19 00:00:12 -0300

    Site -produto

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $app = new Slim();
 
 $app->config('debug', true); // view error
 
+require_once("functions.php");
 require_once("site.php");
 require_once("admin.php");
 require_once("admin-users.php");
